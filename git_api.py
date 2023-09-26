4a55ebce60e15e3040c5491a93bf575a263224cc
AmidTit <114725214+AmidTit@users.noreply.github.com>
2023-09-26 19:11:31 +1000

    Refactor

--- git_api.py
+++ git_api.py
@@ -1,2 +1,3 @@
 token = input("Вставьте ваш токен Git: ")
 username = input("Вставте ваше имя пользователя Git: ")
+auth = (token, username)
--- test_task.py
+++ test_task.py
@@ -4,59 +4,65 @@ import base64
 import git_api
 
 
+def get_file(file_url):
+        
+        response = requests.get(file_url, auth=git_api.auth)
+        data = response.text
 
-# get data.json
-data_url = "https://api.github.com/repos/thewhitesoft/student-2023-assignment/contents/data.json"
-response1 = requests.get(data_url, auth=(git_api.username, git_api.token))
-
-data = response1.text
-json_data = json.loads(data)
-
-decoded_bytes = base64.b64decode(json_data["content"])
-decoded_str = decoded_bytes.decode("ascii")
-data_json = json.loads(decoded_str)
-
-with open("data.json", "w" ) as file:
-    json.dump(data_json, file, indent=2)
+        return json.loads(data)
 
 
-# get replacement.json
-replacement_url = "https://api.github.com/repos/thewhitesoft/student-2023-assignment/contents/replacement.json"
-response2 = requests.get(replacement_url, auth=(git_api.username, git_api.token))
+def decode_file(json_data):
 
-data = response2.text
-json_data = json.loads(data)
+        decoded_bytes = base64.b64decode(json_data["content"])
+        decoded_str = decoded_bytes.decode("ascii")
 
-decoded_bytes = base64.b64decode(json_data["content"])
-decoded_str = decoded_bytes.decode("ascii")
-replacement_json = json.loads(decoded_str)
+        return json.loads(decoded_str)
+    
 
-with open("replacement.json", "w" ) as file:
-    json.dump(replacement_json, file, indent=2)
+def upload_file(file_name, uploadable_file):
+        with open(file_name + ".json" , "w") as file:
+            json.dump(uploadable_file, file, indent = 2)
+            
+
+def do_replacement(replacements_json, data_json):
+
+    none_set = set()  
+
+    for i in range(-1, -len(replacements_json) - 1 , -1):
+        
+        replacement = replacements_json[i]["replacement"]
+        source = replacements_json[i]["source"]
+
+        for j in range(len(data_json)):
+
+            string = data_json[j]
+
+            if replacement in string and source != None:
+
+                changed_string = string.replace(replacement, source)
+                data_json[j] = data_json[j].replace(string, changed_string)
+
+            elif replacement in string and source == None:
+
+                none_set.add(string)
+
+    return [item for item in data_json if item not in none_set]                 
 
 
-   
-# doing replacements
-none_set = set()
+# Точка входа в программу
+if __name__ == "__main__":
 
-for i in range(-1, -len(replacement_json) - 1 , -1):
-    replacement = replacement_json[i]["replacement"]
-    source = replacement_json[i]["source"]
+    data_url = "https://api.github.com/repos/thewhitesoft/student-2023-assignment/contents/data.json"
+    data_json = decode_file(get_file(data_url))
+    upload_file("data", data_json)
 
-    for j in range(len(data_json)):
-        string = data_json[j]
+    
+    replacements_url = "https://api.github.com/repos/thewhitesoft/student-2023-assignment/contents/replacement.json"
+    replacements_json = decode_file(get_file(replacements_url))
+    upload_file("replacement", replacements_json)
 
-        if replacement in string and source != None:
-            new_string = string.replace(replacement, source)
-            data_json[j] = data_json[j].replace(string, new_string)
-
-        elif replacement in string and source == None:
-            none_set.add(string)
-
-result = [item for item in data_json if item not in none_set]
-
-#upload the result.json
-with open("result.json", "w") as f:
-    json.dump(result, f, indent=2)
+    result = do_replacement(replacements_json, data_json)
+    upload_file("result", result)
 
    
